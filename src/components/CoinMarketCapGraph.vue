dd12ac4748a72dda15fd89e171757f54c138928a
DomenicRoti <domenic.roti@gmail.com>
2021-07-13 16:38:58 -0600

    - Adding a doughnut graph to show market cap. - Moving the coin table into it's own component.

---
+++ src/components/CoinMarketCapGraph.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="chart-container">
+    <vue3-chart-js
+      :type="doughnutChart.type"
+      :data="doughnutChart.data"
+      :options="doughnutChart.options"
+      ref="chartRef"
+    />
+  </div>
+</template>
+
+<script>
+import { computed, ref, reactive, watchEffect } from 'vue'
+import { useStore } from 'vuex'
+import Vue3ChartJs from "@j-t-mcc/vue3-chartjs";
+
+export default {
+  name: 'CoinTable',
+  components: {
+    Vue3ChartJs,
+  },
+  setup () { 
+    const store = useStore()
+    const chartRef = ref(null)
+    const doughnutChart = reactive({
+      type: "doughnut",
+      data: {
+        datasets: [{
+          label: 'Coins By Market Cap',
+          hoverOffset: 4
+        }],
+      },
+      options: {
+        interaction: {
+          caretSize: 0,
+        },
+        plugins: {
+          title: {
+            display: true,
+            text: 'Top 10 Coins by Market Cap'
+          },
+          tooltip: {
+            callbacks: {
+              label: function(item) {
+                return `${item.label}: $${item.formattedValue}`;
+              }
+            }
+          }
+        }
+      }
+    });
+
+    const getDynamicColor = () => {
+      const r = Math.floor(Math.random() * 255);
+      const g = Math.floor(Math.random() * 255);
+      const b = Math.floor(Math.random() * 255);
+      return `rgb(${r},${g},${b})`
+    };
+
+    const getChartData = (coins) => {
+      const topTenCoins = coins.splice(0, 10)
+      doughnutChart.data.datasets[0].data = topTenCoins.map(coin => coin.market_cap)
+      doughnutChart.data.labels = topTenCoins.map(coin => coin.symbol)
+      doughnutChart.data.datasets[0].backgroundColor = topTenCoins.map(() => getDynamicColor())
+      if (!chartRef.value) {
+        return
+      }
+      chartRef.value.update(250)
+    }
+
+    const coins = computed(() => store.state.coins.coins)
+
+    watchEffect(() => {
+      if (coins.value.length) {
+        getChartData([...coins.value])
+      }
+    })
+    
+    return {
+      coins,
+      chartRef,
+      doughnutChart,
+      getChartData,
+      getDynamicColor
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.chart-container {
+  max-width: 400px;
+  margin: 0 auto 15px;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,86 +1,21 @@
 <template>
     <h1>Dashboard</h1>
-    
-    <table v-if="coins.length">
-      <tr>
-        <th>Name</th>
-        <th>Price</th>
-        <th>% Change</th>
-      </tr>
-      <tr 
-        v-for="coin in coins"
-        :key="coin.id"
-        class="coin-row"
-      >
-        <td>
-          <router-link
-            :to="{ name: 'Listing', params: { id: coin.id },}"
-          >
-            <div class="flex">
-              <img :src="coin.image" />
-              <span class="flex name">
-                <span>{{ coin.name}}</span>
-                <span>{{ Formatting.uppercase(coin.symbol) }}</span>
-              </span>
-              
-            </div>
-          </router-link>
-        </td>
-        <td>
-          {{Formatting.formatDollar(coin.current_price)}}
-        </td>
-        <td>
-          {{coin.price_change_percentage_24h}}%
-        </td>
-      </tr>
-    </table>
-    <span v-else>Loading</span>
+    <coin-market-cap-graph />
+    <coin-table />
 </template>
 
 <script>
-import { computed } from 'vue'
-import { useStore } from 'vuex'
-import Formatting from '@/utils/formatting'
+import CoinTable from '@/components/CoinTable'
+import CoinMarketCapGraph from '@/components/CoinMarketCapGraph'
 
 export default {
   name: 'Dashboard',
-  setup () {
-    const store = useStore()
-    return {
-      coins: computed(() => store.state.coins.coins),
-      Formatting
-    }
+  components: {
+    CoinMarketCapGraph,
+    CoinTable
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  table {
-    margin: auto;
-  }
-  td {
-    padding-bottom: 15px;
-    padding-right: 15px;
-  }
-  td, th {
-    text-align: left;
-  }
-  .coin-row {
-    color: $cg-black;
-    
-    .flex {
-      flex-direction: row;
-      align-items: center;
-    }
-
-    .name {
-      flex-direction: column;
-      flex-grow: 1;
-    }
-
-    img {
-      max-width: 50px;
-      margin-right: 16px;
-    }
-  }
 </style>
